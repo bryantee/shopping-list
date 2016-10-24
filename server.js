d870d2036835755941c26be6c7ce645684880858
bryantee <bryantee@gmail.com>
2016-10-24 15:19:57 -0700

    Add DELETE route/endpoint: need to wire front end

--- server.js
+++ server.js
@@ -41,6 +41,20 @@ app.post('/items', jsonParser, function(req, res) {
   console.log("Received " + itemName);
 });
 
+app.delete('/items/:id', jsonParser, function(req, res) {
+  var id = req.params.id;
+  console.log('Receied request to delete item with id of ' + id);
+  storage.items.forEach(function(item) {
+    console.log('forEach called on storage.items...');
+    console.log('item id is: ' + item.id + ' and name is: ' + item.name);
+    if (item.id == id) {  // <-- Is this a good way to deal with type coercion?
+      console.log('Found item with matching id ' + id);
+      storage.items.splice(item, 1);
+      res.status(200).json(item);
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log('listening on port 3000');
 });
