0e1bf2c2ae65a2a096a7f45921ef5ca6c7681ea1
bryantee <bryantee@gmail.com>
2016-10-24 17:26:39 -0700

    Add PUT route to update / edit items in shopping list

--- server.js
+++ server.js
@@ -59,6 +59,26 @@ app.delete('/items/:id', function(req, res) {
   }
 });
 
+app.put('/items/:id', jsonParser, function(req, res) {
+  var id = parseInt(req.params.id);
+  var body = req.body;
+  var itemName; // the new name
+  var idFound = false;
+  if (req.body.hasOwnProperty("name")) {
+    itemName = req.body.name;
+    storage.items.forEach(function(item, index) {
+      if (item.id === id) {
+        storage.items[index].name = itemName;
+        idFound = true;
+        res.status(200).json(storage.items[index]);
+      }
+    });
+    if (!idFound) {
+      res.sendStatus(400);
+    }
+  }
+});
+
 app.listen(3000, function() {
   console.log('listening on port 3000');
 });
