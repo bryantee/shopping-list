26348593af68cb4a51eff65f900df6a1ff22e5e3
Bryan Swagerty <bryantee@gmail.com>
2016-10-25 12:01:40 -0700

    Update server port for Heroku deploy

--- server.js
+++ server.js
@@ -109,6 +109,4 @@ app.get('/user/:username', function(req, res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
-});
+app.listen(process.env.PORT || 8080, process.env.IP);
