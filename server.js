839f93609cdfdd181c54ceca4db09f0f9306f465
bryantee <bryantee@gmail.com>
2016-11-01 18:51:05 -0700

    Add GET and POST routes

--- server.js
+++ server.js
@@ -6,8 +6,40 @@ const config = require('./config');
 
 const app = express();
 
-app.use((bodyParser.json());
+app.use((bodyParser.json()));
 app.use(express.static('public'));
+
+const Item = require('./models/item');
+
+app.get('/items', function(req, res) {
+  Item.find(function(err, items) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      });
+    }
+    res.status(200).json(items);
+  });
+});
+
+app.post('/items', function(req, res) {
+  Item.create({
+    name: req.body.name
+  }, function(err, item) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      });
+    }
+    res.status(201).json(item);
+  });
+});
+
+app.use('*', function(req, res) {
+  res.status(404).json({
+    message: 'Not found'
+  });
+});
 
 const runServer = function(callback) {
   mongoose.connect(config.DATABASE_URL, function(err) {
